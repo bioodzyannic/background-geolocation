6f2b52196381d5a95a6b2561debf662b327372e8
bioodzyannic <yannic.niessen@gmail.com>
2023-06-09 02:04:54 +0200

    removed faulty buildconfig

--- android/src/main/java/com/equimaps/capacitor_background_geolocation/BackgroundGeolocation.java
+++ android/src/main/java/com/equimaps/capacitor_background_geolocation/BackgroundGeolocation.java
@@ -28,7 +28,6 @@ import com.getcapacitor.PermissionState;
 import com.getcapacitor.Plugin;
 import com.getcapacitor.PluginCall;
 import com.getcapacitor.PluginMethod;
-import com.getcapacitor.android.BuildConfig;
 import com.getcapacitor.annotation.CapacitorPlugin;
 import com.getcapacitor.annotation.Permission;
 import com.getcapacitor.annotation.PermissionCallback;
